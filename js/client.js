a0e7d191e94309d93e50ce8a25fb1b44644fc2a2
lalit1995saini <138833990+lalit1995saini@users.noreply.github.com>
2024-08-10 08:33:29 -1200

    add message tune

--- js/client.js
+++ js/client.js
@@ -3,13 +3,17 @@ const socket = io('http://localhost:8000');
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container");
+var audio = new Audio('ting.mp3.mp3');
 
 const append = (message, position) => {
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
     messageElement.classList.add('message', position);
     messageContainer.append(messageElement);
-    messageContainer.scrollTop = messageContainer.scrollHeight; // Auto-scroll to the bottom
+    messageContainer.scrollTop = messageContainer.scrollHeight; 
+    if(position=='left'){
+        audio.play();
+    }
 }
 
 const name = prompt("Enter your name to join");
